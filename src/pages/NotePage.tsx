ed9d901cca62af89a1ed42e8c360353eea0fcc16
Sailokeshg <18r01a0579@cmritonline.ac.in>
2023-01-18 17:21:54 +0530

    update and delete functionality added

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 // import notes from "../assets/data";
 import { ReactComponent as ArrrowLeft } from "../assets/arrow-left.svg";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 
 const NotePage = () => {
   const [note, setNote] = useState<any>(null);
   let noteId: any = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getNote();
@@ -18,16 +19,48 @@ const NotePage = () => {
   };
   // let note = notes.find((note) => note.id === parseInt(noteId.id));
 
+  const updateNote = async () => {
+    await fetch(`http://localhost:5000/notes/${note.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
+
+  const handleSubmit = () => {
+    updateNote();
+    navigate("/");
+  };
+
+  const deleteNote = async () => {
+    await fetch(`http://localhost:5000/notes/${note.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+    navigate("/");
+  };
+
   return (
     <div className="note">
       <div className="note-header">
         <h3>
           <Link to="/">
-            <ArrrowLeft />
+            <ArrrowLeft onClick={handleSubmit} />
           </Link>
         </h3>
+        <button onClick={deleteNote}>Delete</button>
       </div>
-      <textarea value={note?.body} />
+      <textarea
+        value={note?.body}
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+      />
     </div>
   );
 };
